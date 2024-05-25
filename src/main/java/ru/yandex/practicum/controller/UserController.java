737d53d1a8e59cfeab3991c191ad6583a644c4e3
Oleg Primolennyy <Oleg.Primolennyy@x5.ru>
2024-05-25 16:22:51 +0700

    add endpoint and annotations to handle GET /users http-requests

--- src/main/java/ru/yandex/practicum/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/controller/UserController.java
@@ -1,12 +1,16 @@
 package ru.yandex.practicum.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.model.User;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -20,4 +24,11 @@ public class UserController {
         loginToUser.put(user.getLogin(), user);
         return user;
     }
+
+    @GetMapping
+    public List<User> findAllUsers() {
+        Collection<User> allUsers = loginToUser.values();
+        return new ArrayList<>(allUsers);
+    }
+
 }
