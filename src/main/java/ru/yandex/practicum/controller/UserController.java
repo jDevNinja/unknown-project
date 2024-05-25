6e43743f1f325bafb1465db1ffea4eac6c1dd1bd
Oleg Primolennyy <Oleg.Primolennyy@x5.ru>
2024-05-25 16:20:50 +0700

    add annotations to tell spring how to handle POST /users http-request

--- src/main/java/ru/yandex/practicum/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/controller/UserController.java
@@ -1,15 +1,22 @@
 package ru.yandex.practicum.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.model.User;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
     private final Map<String, User> loginToUser = new HashMap<>();
 
-    public User createUser(User user) {
+    @PostMapping
+    public User createUser(@RequestBody User user) {
         loginToUser.put(user.getLogin(), user);
         return user;
     }
