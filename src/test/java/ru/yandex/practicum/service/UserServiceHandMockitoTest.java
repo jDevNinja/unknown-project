12db7a8a2add4baf9be1889c48ca84ca1378e25e
Oleg Primolennyy <Oleg.Primolennyy@x5.ru>
2024-09-06 20:21:35 +0700

    добавил тест сервиса с ручными моками

---
+++ src/test/java/ru/yandex/practicum/service/UserServiceHandMockitoTest.java
@@ -0,0 +1,27 @@
+package ru.yandex.practicum.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import ru.yandex.practicum.exceptions.UserNotFoundException;
+import ru.yandex.practicum.mappers.UserMapper;
+import ru.yandex.practicum.repository.mappers.UserRepository;
+
+import java.util.Optional;
+
+class UserServiceHandMockitoTest {
+
+  @Test
+  void findUserByLogin_shouldThrowExceptionWhenNotFound() {
+    String login = "absentLogin";
+
+    UserRepository userRepoMock = Mockito.mock(UserRepository.class);
+    UserMapper userMapperMock = Mockito.mock(UserMapper.class);
+    Mockito.when(userRepoMock.findOneByLogin(login)).thenReturn(Optional.empty());
+
+    UserServiceImpl service = new UserServiceImpl(userRepoMock, userMapperMock);
+
+    Assertions.assertThrows(UserNotFoundException.class, () -> service.findUserByLogin(login));
+    Mockito.verify(userRepoMock).findOneByLogin(login);
+  }
+}
