e1c4f9ea1f51763ea04cb945cb050deb75123601
Oleg Primolennyy <Oleg.Primolennyy@x5.ru>
2024-09-06 20:32:15 +0700

    добавил тест сервиса с автоматически внедряемыми моками

---
+++ src/test/java/ru/yandex/practicum/service/UserServiceAutoMockitoTest.java
@@ -0,0 +1,35 @@
+package ru.yandex.practicum.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.yandex.practicum.exceptions.UserNotFoundException;
+import ru.yandex.practicum.mappers.UserMapper;
+import ru.yandex.practicum.repository.mappers.UserRepository;
+
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceAutoMockitoTest {
+
+  @Mock private UserMapper userMapperMock;
+
+  @Mock private UserRepository userRepositoryMock;
+
+  @InjectMocks private UserServiceImpl userService;
+
+  @Test
+  void findUserByLogin_shouldThrowExceptionWhenNotFound() {
+    String login = "absentLogin";
+    Mockito.when(userRepositoryMock.findOneByLogin(login)).thenReturn(Optional.empty());
+
+    Assertions.assertThrows(UserNotFoundException.class, () -> userService.findUserByLogin(login));
+
+    Mockito.verify(userRepositoryMock).findOneByLogin(login);
+    Mockito.verifyNoInteractions(userMapperMock);
+  }
+}
